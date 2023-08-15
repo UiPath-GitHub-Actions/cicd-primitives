649f3d80ce2063110158cefb067326500dc43a72
nikbiri <n.biridzhyan@gmail.com>
2023-08-15 10:18:38 +0100

    Update CiCdFunctions.ps1

--- CiCdFunctions.ps1
+++ CiCdFunctions.ps1
@@ -68,7 +68,7 @@ function GetFolderId([string]$orchestratorApiBaseUrl, [string]$bearerToken, [str
 
 function GetProcessId([string]$orchestratorApiBaseUrl, [string]$bearerToken, [string]$folderId, [string]$processName) {
     $headers = @{"Authorization"="Bearer $($bearerToken)"; "X-UIPATH-OrganizationUnitId"="$($folderId)"}
-    $result = GetOrchApi -bearerToken $bearerToken -headers $headers -uri "$($orchestratorApiBaseUrl)/odata/Releases?%24filter=Name%20eq%20'$($processName)'%20and%20OrganizationUnitId%20eq%20$($folderId)"
+    $result = GetOrchApi -bearerToken $bearerToken -headers $headers -uri "$($orchestratorApiBaseUrl)/odata/Releases?%24filter=Name%20eq%20'$($processName)'"
     return $result.value[0].Id.ToString()
 }
 
